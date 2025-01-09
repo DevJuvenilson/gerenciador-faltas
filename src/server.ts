3217e537fd52044f9f1b223ae236af5c59316bf2
Daniel Ribeiro <109773063+DevJuvenilson@users.noreply.github.com>
2025-01-09 17:15:46 -0300

    Create api structure

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,8 @@
-console.log("Hello World")
+import createApp from "./app";
+
+const app = createApp();
+const port = process.env.PORT;
+
+app.listen(port, () => {
+    console.log(`🚀 Server started on port http://localhost:${port}`);
+})
